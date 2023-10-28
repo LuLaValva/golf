ae6726830517caaaaa64357c3230f0d2d163f10d
LuLaValva <lukelavalva@gmail.com>
2023-10-28 16:28:51 -0700

    remove scrollbars and edit homepage

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,9 +1,20 @@
 import { A } from "@solidjs/router";
+import styles from "./homepage.module.css";
 
 export default function Home() {
   return (
-    <main>
-      <h1>Golf!</h1>
+    <main class={styles.main}>
+      <h1 class={styles.title}>
+        G
+        <span class={styles.ball}>
+          <img
+            src="/ball.svg"
+            alt='Golf ball used to represent an "o"'
+            class={styles.ball}
+          />
+        </span>
+        lf!
+      </h1>
       <A href="/editor">Course editor</A>
     </main>
   );
